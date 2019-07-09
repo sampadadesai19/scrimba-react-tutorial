7a6eefede8ed6fcad3016aef0fbe4f4343c83e06
Sampada <sampadadesai19@gmail.com>
2019-07-08 21:52:16 -0500

    Add ReactDOM and JSX Practice

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<div><h1>Hello world!</h1><p>This is a paragraph</p></div>, document.getElementById('root'));
+/*ReactDOM.render(<div><h1>Hello world!</h1><p>This is a paragraph</p></div>, document.getElementById('root'));
 
 var myNewP = document.createElement("p");
-myNewP.innerHTML = "This is a paragraph."
+myNewP.innerHTML = "This is a paragraph."*/
+
+/*ReactDOM & JSX Practice
+Render an unordered list to the page. The list should contain 3 list items.
+*/
+
+ReactDOM.render(<ul>
+<li>Cat</li>
+<li>Dog</li>
+<li>Elephant</li>
+</ul>, document.getElementById('root'));
